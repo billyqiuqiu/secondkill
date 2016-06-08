d57377a141bd81bbe27e9617d2f9cd11cd5ddd50
billy <billy.qiu@oracle.com>
2016-06-08 10:00:35 +0800

    mybatis configuration

--- seckill/src/main/java/org/seckill/dao/ISuccessKilledDao.java
+++ seckill/src/main/java/org/seckill/dao/ISuccessKilledDao.java
@@ -1,7 +1,11 @@
 package org.seckill.dao;
 
+import org.seckill.entity.SuccessKilled;
+
 public interface ISuccessKilledDao {
 	
 	int insertSuccessKill(Long seckillId, Long userPhone);
+	
+	SuccessKilled queryByIdWithSeckill(Long seckillId);
 
 }
